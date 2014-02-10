35aed6d96f6f617e2f11970b552bcf7f17bde86f
Gregory Usenko <ebio@ukr.net>
2014-02-10 13:38:41 +0200

    fix block offset

--- wp-content/themes/cabinet/p-catalog.php
+++ wp-content/themes/cabinet/p-catalog.php
@@ -6,14 +6,21 @@ get_header();?>
 <section class="catalog-content">    
     <?php $terms = apply_filters('taxonomy-images-get-terms', NULL, array('taxonomy' => 'product_taxonomy'))?>
     
-    <?php foreach($terms as $term): ?>
-    <article class="catalog-post big mayo">
-        <?php echo wp_get_attachment_image($term->image_id, 'full'); ?>
-        <h3><?php echo $term->name?></h3>
-        <p><?php echo $term->description?></p>
-        <a href="<?php echo get_term_link($term->slug, 'product_taxonomy') ?>" class="btn blue">Подробнее</a>
-    </article><!--catalog post-->
-    <?php endforeach; ?>
+<?php $ariticle_counter = 0; ?>
+<div class="catalog-post-clear">
+<?php foreach($terms as $term): ?>
+	<?php $article_counter++; ?>
+		<article class="catalog-post big mayo">
+			<?php echo wp_get_attachment_image($term->image_id, 'full'); ?>
+			<h3><?php echo $term->name?></h3>
+			<p><?php echo $term->description?></p>
+			<a href="<?php echo get_term_link($term->slug, 'product_taxonomy') ?>" class="btn blue">Подробнее</a>
+		</article><!--catalog post-->
+	<?php if($article_counter == 2) {?>
+		</div><div class="catalog-post-clear"> 
+	<?php $article_counter = 0; }?>
+<?php endforeach; ?>
+</div>
 
 </section><!--catalog content-->
 <?php get_footer(); ?>
